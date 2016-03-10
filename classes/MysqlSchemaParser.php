0879c48661ac6261c849950eb9cb47f34f5ec828
Eissa Soubhi <eissa.soubhi@gmail.com>
2016-03-10 21:41:53 +0100

    Update MysqlSchemaParser.php

--- classes/MysqlSchemaParser.php
+++ classes/MysqlSchemaParser.php
@@ -8,6 +8,7 @@ class MysqlSchemaParser
 {
     protected $mysql_schema;
     protected $parser;
+    protected $create_table_pattern = "/CREATE +(?:TEMPORARY +)?TABLE +(?:IF +NOT +EXISTS)?/is";
     function __construct($mysql_schema)
     {
         $this->setSchema($mysql_schema);
@@ -23,8 +24,21 @@ class MysqlSchemaParser
     {
         if ($this->hasSchema())
         {
+            $create_table_schemas = array();
+
+            $schemas = preg_split($this->create_table_pattern,
+                                    $this->mysql_schema, null ,PREG_SPLIT_DELIM_CAPTURE);
+            // var_dump($schemas);
+                // exit(); "<hr>";
+            foreach ($schemas as $key => $table_body)
+            {
+                if($table_body)
+                {
+                    $create_table_schemas[] = "CREATE TABLE ".$table_body;
+                }
+            }
             ini_set('xdebug.var_display_max_depth', 10);
-            var_dump($this->parser);
+            var_dump($create_table_schemas);
             exit();
         }
         else
